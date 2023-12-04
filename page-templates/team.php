ce336921d4de9205c9bb973a9063d7087dd0860c
Marvin Okafor <marvinoka4@gmail.com>
2023-12-04 00:03:32 +0000

    update team bio

--- page-templates/team.php
+++ page-templates/team.php
@@ -31,10 +31,32 @@ get_header(); ?>
                 <div class="card-section">
                   <h4>Gifty S Adrah </h4>
                   <p class="margin-bottom-0">CEO</p>
-                  <p class="margin-bottom-2">KWBEL Ghana</p>
+                  <p class="margin-bottom-0">KWBEL Ghana</p>
+                  <a data-open="giftya" class="margin-bottom-2 primary">Read Bio <i class="uil uil-info-circle"></i></a>
                 </div>
               </div>
             </div>
+
+          <div class="reveal" id="giftya" data-reveal>
+            <div class="grid-x grid-margin-x">
+              <div class="medium-4 cell">
+                <div class="profile-photo">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/photos/team-member-gift.jpg" alt="Gifty" style="padding-top: 30px">
+                </div>
+              </div>
+              <div class="medium-8 cell">
+                <p class="margin-bottom-0"><b>Gifty</b></p>
+
+                <p class="text-small margin-bottom-0">CEO</p>
+                <hr>
+                <p class="margin-bottom-3">Gifty is a well-experienced business administrator and developer with valuable industry experience and a passion for management. Results-oriented with a proven track record of market investment position of a company and maximizing opportunities for financial growth in the real estate and gold markets. She's adept at analytical thinking, strategic planning, leadership, and building solid relationships with business partners. She obtained a BSc in business administration and an HND in computer software engineering. She's also an actress.</p>
+              </div>
+            </div>
+            <button class="close-button" data-close aria-label="Close modal" type="button">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
 
           <div class="medium-3 small-12 cell align-center text-center">
             <div class="card">
@@ -42,10 +64,32 @@ get_header(); ?>
               <div class="card-section">
                 <h4>Bright N Uzoma </h4>
                 <p class="margin-bottom-0">Consultant</p>
-                <p class="margin-bottom-2">KWBEL</p>
+                <p class="margin-bottom-0">KWBEL</p>
+                <a data-open="brightu" class="margin-bottom-2 primary">Read Bio <i class="uil uil-info-circle"></i></a>
               </div>
             </div>
           </div>
+
+          <div class="reveal" id="brightu" data-reveal>
+            <div class="grid-x grid-margin-x">
+              <div class="medium-4 cell">
+                <div class="profile-photo">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/photos/team-member-bright.jpg" alt="Bright" style="padding-top: 30px">
+                </div>
+              </div>
+              <div class="medium-8 cell">
+                <p class="margin-bottom-0"><b>Bright</b></p>
+
+                <p class="text-small margin-bottom-0">Consultant</p>
+                <hr>
+                <p class="margin-bottom-3">Bright is an accountant and auditor and has varied experiences across industries. He has over five years of post-qualification experience in accounting and auditing and exposure to financial services, oil and gas, construction, retail and manufacturing industries, and not-for-profit organizations. Bright has also reviewed organizational structures, evaluated internal control systems, and installed and documented procedures for various organizations and, most recently, the construction industry. He is an accounting graduate from Abia State University Nigeria and a professional-level Institute of Chartered Accountants of Nigeria (ICAN) student. He has over three years of training experience with G.E Osagie (Chartered Accountants), a proud member of Alliot Global Alliance.</p>
+              </div>
+            </div>
+            <button class="close-button" data-close aria-label="Close modal" type="button">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
 
             <div class="medium-3 small-12 cell align-center text-center">
               <div class="card">
@@ -53,10 +97,32 @@ get_header(); ?>
                 <div class="card-section">
                   <h4>Gift O Akujim </h4>
                   <p class="margin-bottom-0">Manager</p>
-                  <p class="margin-bottom-2">KWBEL Nigeria</p>
+                  <p class="margin-bottom-0">KWBEL Nigeria</p>
+                  <a data-open="gifto" class="margin-bottom-2 primary">Read Bio <i class="uil uil-info-circle"></i></a>
                 </div>
               </div>
             </div>
+
+          <div class="reveal" id="gifto" data-reveal>
+            <div class="grid-x grid-margin-x">
+              <div class="medium-4 cell">
+                <div class="profile-photo">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/photos/team-member-onyinyechi.jpg" alt="Gift" style="padding-top: 30px">
+                </div>
+              </div>
+              <div class="medium-8 cell">
+                <p class="margin-bottom-0"><b>Gift</b></p>
+
+                <p class="text-small margin-bottom-0">Manager</p>
+                <hr>
+                <p class="margin-bottom-3">Gift is an experienced architect and realtor driven by passion. She takes pride in providing the best service (property or building plan) possible. As a real estate consultant and manager at Kings World Business Empire Limited (KWBEL), her objectives include giving investment and financial advice to clients who want to buy, sell, or invest in a property, helping their clients navigate the housing and property markets, and providing insight into current trends. Gift has received recognition from KWBEL for her extraordinary dedication to assisting the client in obtaining their dream property in addition to her primary job duties.</p>
+              </div>
+            </div>
+            <button class="close-button" data-close aria-label="Close modal" type="button">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
 
             <div class="medium-3 small-12 cell align-center text-center">
               <div class="card">
@@ -64,9 +130,30 @@ get_header(); ?>
                 <div class="card-section">
                   <h4>Tochukwu Ogbonna </h4>
                   <p class="margin-bottom-0">Media Coordinator</p>
-                  <p class="margin-bottom-2">KWBEL</p>
+                  <p class="margin-bottom-0">KWBEL</p>
+                  <a data-open="tochukwuo" class="margin-bottom-2 primary">Read Bio <i class="uil uil-info-circle"></i></a>
                 </div>
               </div>
+            </div>
+
+            <div class="reveal" id="tochukwuo" data-reveal>
+              <div class="grid-x grid-margin-x">
+                <div class="medium-4 cell">
+                  <div class="profile-photo">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/photos/team-member-tochukwu.jpg" alt="Tochukwu" style="padding-top: 30px">
+                  </div>
+                </div>
+                <div class="medium-8 cell">
+                  <p class="margin-bottom-0"><b>Tochukwu</b></p>
+
+                  <p class="text-small margin-bottom-0">Media Coordinator</p>
+                  <hr>
+                  <p class="margin-bottom-3">Tochukwu specializes in project coordination and ICT administration and has accurate data storage skills with over four years of experience. He has a proven knowledge of ICT Administration, Graphic Design, Human behaviour and performance, Conflict resolution, Business Administration, and Holistic Learning. He is a graduate of Economics from the prestigious Abia State University, Uturu, Abia State, Nigeria. He hails from Abia State, Isialangwa North LGA, to be precise, and Lagos State, Nigeria. He enjoys engaging himself with graphic design and surfing the internet. Essentially, he is an introvert who loves having his alone time, which helps him think better and more creatively.</p>
+                </div>
+              </div>
+              <button class="close-button" data-close aria-label="Close modal" type="button">
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
 
 
